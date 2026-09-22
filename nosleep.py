261a8811d1f4de03657cbc7cb62107bc54c6ae60
Henri Tarelli <ht2803@nyu.edu>
2026-09-22 19:29:53 -0400

    fixes instances of caffeinate being created when pressing turn on multiple times

--- nosleep.py
+++ nosleep.py
@@ -7,16 +7,24 @@ class AwesomeStatusBarApp(rumps.App):
     def prefs(self, _):
         rumps.alert("jk! no preferences available!")
 
+
+#this part has an issue where if u press the button mutiple times, it will start multiple caffeinate processes. 
     @rumps.clicked("Turn On NoSleep Indefinitely") #turns on NoSleep indefinitely (until the user toggles it off)
     def turn_on(self, sender):
-            subprocess.Popen(["caffeinate", "-d"])
+            self.caffeinate_process = subprocess.Popen(["caffeinate", "-d"])
             sender.state = True
             rumps.alert("NoSleep Toggled on") 
+            if rumps.clicked("Turn On NoSleep Indefinitely") and sender.state == True:  #should fix later bc this closes all available caffeinate processes, but for now it works
+                pkill = subprocess.Popen(["pkill", "caffeinate"])
+                pkill.wait()
+                subprocess.Popen(["caffeinate", "-d"])
 
-            
+    #this part works fine as is
     @rumps.clicked("Turn Off NoSleep") #turns off NoSleep (until the user toggles it on)
     def turn_off(self, sender):
-            subprocess.run(["pkill", "caffeinate"])
+            self.caffeinate_process.terminate()
+            self.caffeinate_process.wait()
+            self.caffeinate_process = None
             sender.state = False
             rumps.alert("NoSleep Toggled off")
 
